6a976c01523a17c5db37b300c82d2f9b3f4b052e
Vigneswar-A <ugiv.rawsengiv@gmail.com>
2024-07-18 17:40:03 +0530

    added listing

---
+++ static/script/listing.js
@@ -0,0 +1,40 @@
+async function getListing() {
+    try {
+        const response = await fetch(`https://api.github.com/repos/vigneswar-a/vigneswar-a.github.io/contents/${document.location.pathname.replace('.html', '')}`);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+}
+
+async function makeCard(name, path) {
+    let card = document.createElement('a');
+    card.innerText = name;
+    card.href = path;
+    return card;
+}
+
+async function renderCards() {
+    let content = document.getElementById("content");
+
+    try {
+        const list = await getListing();
+
+        for (let item of list) {
+            let { name, path } = item;
+            let card = await makeCard(name.replace('.pdf', '').replace(/_/g, ' '), path);
+            content.appendChild(card);
+        }
+    } catch (error) {
+        console.error('Error rendering cards:', error);
+    }
+}
+
+renderCards();
